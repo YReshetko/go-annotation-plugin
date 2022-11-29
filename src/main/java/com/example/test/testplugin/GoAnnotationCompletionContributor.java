e95619b7ffcf8d1f4c61e3a123830de271e52a86
Yury Rash <yrashetska@gmail.com>
2022-11-29 19:41:47 +0300

    Update annotations list

--- src/main/java/com/example/test/testplugin/GoAnnotationCompletionContributor.java
+++ src/main/java/com/example/test/testplugin/GoAnnotationCompletionContributor.java
@@ -22,8 +22,36 @@ public class GoAnnotationCompletionContributor extends CompletionContributor {
           @NotNull final ProcessingContext context,
           @NotNull final CompletionResultSet result
       ) {
-        result.addElement(LookupElementBuilder.create("My completion for go-annotation", "Short"));
-        result.addElement(LookupElementBuilder.create("My another option", "Another"));
+        result.addElement(LookupElementBuilder.create("Constructor", "@Constructor"));
+        result.addElement(LookupElementBuilder.create("Constructor...", "@Constructor(name=\"New{{.TypeName}}\", type=\"struct\", exported=\"true\")"));
+
+        result.addElement(LookupElementBuilder.create("Optional", "@Optional"));
+        result.addElement(LookupElementBuilder.create("Optional...", "@Optional(name=\"{{.TypeName}}Option\", constructor=\"New{{.TypeName}}\", with=\"With{{.FieldName}}\", type=\"struct\", exported=\"true\")"));
+
+        result.addElement(LookupElementBuilder.create("Builder", "@Builder"));
+        result.addElement(LookupElementBuilder.create("Builder...", "@Builder(name=\"{{.TypeName}}Builder\", constructor=\"New{{.TypeName}}Builder\", build=\"{{.FieldName}}\", terminator=\"Build\", type=\"struct\", exported=\"true\")"));
+
+        result.addElement(LookupElementBuilder.create("PostConstruct", "@PostConstruct"));
+        result.addElement(LookupElementBuilder.create("PostConstruct...", "@PostConstruct(priority=\"1\")"));
+
+        result.addElement(LookupElementBuilder.create("Init", "@Init"));
+        result.addElement(LookupElementBuilder.create("Init...", "@Init(len=\"-1\", cap=\"-1\")"));
+
+        result.addElement(LookupElementBuilder.create("Exclude", "@Exclude"));
+
+
+        result.addElement(LookupElementBuilder.create("Mapper", "@Mapper"));
+        result.addElement(LookupElementBuilder.create("Mapper...", "@Mapper(name=\"{{.TypeName}}Impl\")"));
+
+        result.addElement(LookupElementBuilder.create("Mapping...", "@Mapping(target=\"\", source=\"\")"));
+        result.addElement(LookupElementBuilder.create("SliceMapping...", "@SliceMapping(target=\"\", source=\"\")"));
+        result.addElement(LookupElementBuilder.create("MapMapping...", "@MapMapping(target=\"\", source=\"\")"));
+        result.addElement(LookupElementBuilder.create("IgnoreDefaultMapping", "@IgnoreDefaultMapping"));
+
+        result.addElement(LookupElementBuilder.create("Mock", "@Mock"));
+        result.addElement(LookupElementBuilder.create("Mock...", "@Mock(name=\"{{ .TypeName }}Mock\", sub=\"mocks\")"));
+
+        result.addElement(LookupElementBuilder.create("Rest...", "@Rest(method=\"GET\", path=\"/\")"));
       }
     });
     //Messages.showInfoMessage("Running from go-annotation processor", "go-annotation");
